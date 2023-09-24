7421d7f5db3128012fc80301b1f285747cb06505
spiritualsection2703 <88161451+spiritualsection2703@users.noreply.github.com>
2023-09-24 19:07:57 -0400

    Add files via upload

--- Gradebook.java
+++ Gradebook.java
@@ -15,9 +15,10 @@ public class Gradebook {
         listOfStudents.add(student);
     }
     
+    // This is the main #1
     public int calculateMinScore() {
         if (listOfStudents.isEmpty()) {
-            throw new IllegalStateException("Please add students in the gradebook.");
+            throw new IllegalStateException("Please add students to the gradebook.");
         }
 
         int minScore = listOfStudents.get(0).getGrade().getScore();
@@ -30,6 +31,7 @@ public class Gradebook {
         return minScore;
     }
 
+    //This is the main #2
     public double calculateAvg() {
         double sum = 0;
         for (Student s : listOfStudents)
@@ -37,6 +39,7 @@ public class Gradebook {
         return sum / listOfStudents.size();
     }
 
+    //This is the main #3
     public float calculateMedian() {
         int i = 0, n = listOfStudents.size();
         int[] scores = new int[n];
@@ -49,23 +52,37 @@ public class Gradebook {
             return scores[n / 2];
     }
 
+    // This is the main #4 NEEDS TO BE FIXED
     public String calculateMinLetterGrade() {
     if (listOfStudents.isEmpty()) {
         throw new IllegalStateException("No students in the gradebook.");
     }
 
-    String minLetterGrade = listOfStudents.get(0).getGrade().getLetterGrade();
+    String minLetterGrade = null;
+
     for (Student s : listOfStudents) {
         String letterGrade = s.getGrade().getLetterGrade();
-        if (letterGrade.compareTo(minLetterGrade) < 0) {
+        
+        // Ignore students with null letter grades
+        if (letterGrade == null) {
+            continue;
+        }
+
+        // If minLetterGrade is null or letterGrade is smaller, update minLetterGrade
+        if (minLetterGrade == null || letterGrade.compareTo(minLetterGrade) < 0) {
             minLetterGrade = letterGrade;
         }
     }
+
+    if (minLetterGrade == null) {
+        throw new IllegalStateException("No valid minimum letter grade found.");
+    }
+
     return minLetterGrade;
 }
 
-
-public String calculateMaxLetterGrade() {
+    //This is the main #5 NEEDS TO BE FIXED
+    public String calculateMaxLetterGrade() {
     if (listOfStudents.isEmpty()) {
         throw new IllegalStateException("No students in the gradebook.");
     }
@@ -77,14 +94,16 @@ public String calculateMaxLetterGrade() {
             maxLetterGrade = letterGrade;
         }
     }
-    return maxLetterGrade;
-}
+        return maxLetterGrade;
+    }
 
+    //This is the main #6
     public void printAllStudents() {
         for (Student s : listOfStudents)
             System.out.printf("%s\t%s\t%d\t%d\n", s.getFirstName(), s.getLastName(), s.getPid(), s.getGrade().getScore());
     }
     
+    //this is the main #7 Output displays "null"
     public void findAndPrintLetterGrade(int targetPid) {
     for (Student s : listOfStudents) {
         if (s.getPid() == targetPid) {
@@ -94,21 +113,23 @@ public String calculateMaxLetterGrade() {
         }
     }
     
-    System.out.println("Student with PID " + targetPid + " not found in the gradebook.");
-}
+        System.out.println("Student with PID " + targetPid + " not found in the gradebook.");
+    }
     
+    //This is the main #8
     public void findAndPrintFullName(int targetPid) {
     for (Student s : listOfStudents) {
         if (s.getPid() == targetPid) {
             String fullName = s.getFirstName() + " " + s.getLastName();
-            System.out.println("Full Name for PID " + targetPid + ": " + fullName);
+            System.out.println("Student's PID " + targetPid + ": " + fullName);
             return; 
         }
     }
     
     System.out.println("Student with PID " + targetPid + " not found in the gradebook.");
-}
+    }
     
+    //This is the main #9 newGradeSymbol change it to string not char?
     public void updateStudentGrade(int targetPid, String newGradeSymbol) {
     for (Student s : listOfStudents) {
         if (s.getPid() == targetPid) {
@@ -119,61 +140,65 @@ public String calculateMaxLetterGrade() {
     }
     
     System.out.println("Student with PID " + targetPid + " not found in the gradebook.");
-}
-
-public char calculateAverageLetterGrade() {
-    if (listOfStudents.isEmpty()) {
-        throw new IllegalStateException("No students in the gradebook.");
     }
 
-    double averageScore = calculateAvg(); 
+    //This is the main #10 This works
+    public char calculateAverageLetterGrade() {
+        if (listOfStudents.isEmpty()) {
+            throw new IllegalStateException("No students in the gradebook.");
+        }
+
+        double averageScore = calculateAvg(); 
 
 
-    if (averageScore >= 90.0) {
-        return 'A';
-    } else if (averageScore >= 80.0) {
-        return 'B';
-    } else if (averageScore >= 70.0) {
-        return 'C';
-    } else if (averageScore >= 60.0) {
-        return 'D';
-    } else {
-        return 'F';
+        if (averageScore >= 90.0) {
+            return 'A';
+        } else if (averageScore >= 80.0) {
+            return 'B';
+        } else if (averageScore >= 70.0) {
+            return 'C';
+        } else if (averageScore >= 60.0) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }    
+
+     //This is the main #11
+    public char calculateMedianLetterGrade() {
+        if (listOfStudents.isEmpty()) {
+            throw new IllegalStateException("No students in the gradebook.");
+        }
+
+        float medianScore = calculateMedian();
+
+        if (medianScore >= 90.0f) {
+            return 'A';
+        } else if (medianScore >= 80.0f) {
+            return 'B';
+        } else if (medianScore >= 70.0f) {
+            return 'C';
+        } else if (medianScore >= 60.0f) {
+            return 'D';
+        } else {
+            return 'F';
+        }
     }
-}    
-    
-public char calculateMedianLetterGrade() {
-    if (listOfStudents.isEmpty()) {
-        throw new IllegalStateException("No students in the gradebook.");
+
+    //This is the main #12
+    public void printStudentTable() {
+        System.out.println("firstname\tlastname\tPID\tscore");
+        for (Student s : listOfStudents) {
+            System.out.printf("%s\t%s\t%d\t%d\n", s.getFirstName(), s.getLastName(), s.getPid(), s.getGrade().getScore());
+        }
     }
 
-    float medianScore = calculateMedian();
-
-    if (medianScore >= 90.0f) {
-        return 'A';
-    } else if (medianScore >= 80.0f) {
-        return 'B';
-    } else if (medianScore >= 70.0f) {
-        return 'C';
-    } else if (medianScore >= 60.0f) {
-        return 'D';
-    } else {
-        return 'F';
+    //This is the main #13 showing TWICE OF EVERYTHING AND shows null in letter grades
+    public void printStudentTableWithLetterGrades() {
+        System.out.println("firstname\tlastname\tPID\tletter-grades");
+        for (Student s : listOfStudents) {
+            System.out.printf("%s\t%s\t%d\t%c\n", s.getFirstName(), s.getLastName(), s.getPid(), s.getGrade().getLetterGrade());
+        }
     }
-}
-
-public void printStudentTable() {
-    System.out.println("firstname\tlastname\tPID\tscore");
-    for (Student s : listOfStudents) {
-        System.out.printf("%s\t%s\t%d\t%d\n", s.getFirstName(), s.getLastName(), s.getPid(), s.getGrade().getScore());
-    }
-}
-
-public void printStudentTableWithLetterGrades() {
-    System.out.println("firstname\tlastname\tPID\tletter-grades");
-    for (Student s : listOfStudents) {
-        System.out.printf("%s\t%s\t%d\t%c\n", s.getFirstName(), s.getLastName(), s.getPid(), s.getGrade().getLetterGrade());
-    }
-}
 
 }
--- Main.java
+++ Main.java
@@ -4,44 +4,108 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        Gradebook gradebook = new Gradebook();
+
         System.out.println("Welcome to my grade book!");
-        System.out.println("Please enter the information of the first student using the following format:");
+        System.out.println("Please enter the information of the students using the following format:");
         System.out.println("\"firstName lastName PID grade\"");
-        System.out.println("Please enter 'Done' when you are done.");
+        System.out.println("Enter 'Quit' to finish or 'Calculate Min Score' to find the minimum score.");
     
-        Student student = new Student();
-        
-while (true) {
-        String input = scanner.nextLine();
-        if (input.equalsIgnoreCase("quit")) {
-            System.out.println("Thank you for using our program!");
-            break;
-        } else if (input.equalsIgnoreCase("done")) {
-            System.out.println("Enter the information for the next student or 'Quit' to finish.");
-        } else {
-            String[] parts = input.split(" ");
-            if (parts.length == 4) {
-                String firstName = parts[0];
-                String lastName = parts[1];
-                int pid = Integer.parseInt(parts[2]);
-                int gradeValue = Integer.parseInt(parts[3]);
-
-                Grade grade = new Grade(gradeValue);
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("Quit")) {
+                System.out.println("Thank you for using our program!");
+                break;
+            } else if (input.equalsIgnoreCase("1. Calculate Min Score")) {
                 
-                student.setFirstName(firstName);
-                student.setLastName(lastName);
-                student.setPid(pid);
-                student.setGrade(grade);
-
+                try {
+                    int minScore = gradebook.calculateMinScore();
+                    System.out.println("Minimum Score: " + minScore);
+                } catch (IllegalStateException e) {
+                    System.out.println(e.getMessage());
+                }
+            } else if (input.equalsIgnoreCase("2. Calculate Avg")) {
+                // This calculates and prints the average score
+                double avgScore = gradebook.calculateAvg();
+                System.out.println("Average Score: " + avgScore);
+                // This calculates and prints the Median
+            } else if (input.equalsIgnoreCase("3. Calculate Median")) {
+                float medianScore = gradebook.calculateMedian();
+                System.out.println("Median Score: " + medianScore);
+            } else if (input.equalsIgnoreCase("4. Calculate Min Letter Grade")) {
+                // Calculate and print the minimum letter grade
+                try {
+                    String minLetterGrade = gradebook.calculateMinLetterGrade();
+                    System.out.println("Minimum Letter Grade: " + minLetterGrade);
+                } catch (IllegalStateException e) {
+                    System.out.println(e.getMessage());
+                    }
+                } else if (input.equalsIgnoreCase("5. Calculate Max Letter Grade")) {
+                // Calculate and print the maximum letter grade
+                try {
+                    String maxLetterGrade = gradebook.calculateMaxLetterGrade();
+                    System.out.println("Maximum Letter Grade: " + maxLetterGrade);
+                } catch (IllegalStateException e) {
+                    System.out.println(e.getMessage());
+                    }
+                } else if (input.equalsIgnoreCase("6. Print All Students")) {
+                // Prints students information
+                gradebook.printAllStudents();
+                } else if (input.equalsIgnoreCase("7. Find and Print Letter Grade")) {
+                // Find and print letter grade for the Student's PID
+                System.out.println("Enter the PID to find the letter grade:");
+                int targetPid = scanner.nextInt();
+                scanner.nextLine();
+                gradebook.findAndPrintLetterGrade(targetPid);
+                } else if (input.equalsIgnoreCase("8. Find and Print Full Name")) {
+                // Find and print full name for a specific PID
+                System.out.println("Enter the PID to find the full name:");
+                int targetPid = scanner.nextInt();
+                scanner.nextLine();
+                gradebook.findAndPrintFullName(targetPid);
                 
-                System.out.println("Student added: " + student.getFirstName() + " " + student.getLastName());
-                System.out.println("Please add another student or quit the program.");
+                /*number #9 goes here dont forget that*/
+                
+                } else if (input.equalsIgnoreCase("10. Calculate Average Letter Grade")) {
+                // Calculate and print the average letter grade for all students
+                char avgLetterGrade = gradebook.calculateAverageLetterGrade();
+                System.out.println("Average Letter Grade: " + avgLetterGrade);
+                } else if (input.equalsIgnoreCase("11. Calculate Median Letter Grade")) {
+                // Calculate and print the median letter grade for all students
+                char medianLetterGrade = gradebook.calculateMedianLetterGrade();
+                System.out.println("Median Letter Grade: " + medianLetterGrade);
+                } else if (input.equalsIgnoreCase("12. Print Student Table")) {
+                // Print a table of students' information
+                gradebook.printStudentTable();
+                } else if (input.equalsIgnoreCase("13. Print Student Table with Letter Grades")) {
+                // Print a table of students' information with letter grades
+                System.out.println("Firstname\tLastname\tPID\tLetter Grade");
+                gradebook.printStudentTableWithLetterGrades();
             } else {
-                System.out.println("Please use: \"firstName lastName PID grade\"");
+                String[] parts = input.split(" ");
+                if (parts.length == 4) {
+                    String firstName = parts[0];
+                    String lastName = parts[1];
+                    int pid = Integer.parseInt(parts[2]);
+                    int gradeValue = Integer.parseInt(parts[3]);
+
+                    Grade grade = new Grade(gradeValue);
+                    Student student = new Student();
+
+                    student.setFirstName(firstName);
+                    student.setLastName(lastName);
+                    student.setPid(pid);
+                    student.setGrade(grade);
+
+                    gradebook.addStudent(student);
+
+                    System.out.println("Student added: " + student.getFirstName() + " " + student.getLastName());
+                    System.out.println( "Please type one of the following \n 1. Calculate Min Score \n 2. Calculate Avg \n 3. Calculate Median \n 4. Calculate Min Letter Grade \n 5. Calculate Max Letter Grade \n 6. Print All Students \n 7. Find and Print Letter Grade \n 8. Find and Print Full Name \n 10. Calculate Average Letter Grade \n 11. Calculate Median Letter Grade \n 12. Print Student Table \n 13. Print Student Table with Letter Grades");
+                    System.out.println("Please add another student or quit the program.");
+                } else {
+                    System.out.println("Please use: \"firstName lastName PID grade\"");
+                }
             }
         }
-    }
     }
 }
